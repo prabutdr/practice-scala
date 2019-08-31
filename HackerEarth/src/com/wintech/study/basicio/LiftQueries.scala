70bfdcd39a34c303f901b2448d9e32667282eee4
prabutdr <prabutdr@gmail.com>
2019-08-31 23:07:52 +0530

    Lift queries

---
+++ HackerEarth/src/com/wintech/study/basicio/LiftQueries.scala
@@ -0,0 +1,22 @@
+package com.wintech.study.basicio
+
+import scala.io.StdIn
+
+object LiftQueries extends App {
+  val tc = StdIn.readInt()
+  
+  var (a, b) = (0, 7)  // Lift a at ground floor, Lift b at top floor initially
+  for (t <- 1 to tc) {
+    val rf = StdIn.readInt()
+    val aDist = (a - rf).abs
+    val bDist = (b - rf).abs
+    if (aDist <= bDist) {
+      println("A")
+      a = rf
+    }
+    else {
+      println("B")
+      b = rf
+    }
+  }
+}
