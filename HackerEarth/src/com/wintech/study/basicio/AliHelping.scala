d95dc5d32954fa43009af69560f8fa94df1f35e0
prabutdr <prabutdr@gmail.com>
2019-09-01 12:49:37 +0530

    AliHelping

---
+++ HackerEarth/src/com/wintech/study/basicio/AliHelping.scala
@@ -0,0 +1,19 @@
+package com.wintech.study.basicio
+
+import scala.io.StdIn
+
+object AliHelping extends App {
+  val pattern = """(\d)(\d)(.)(\d)(\d)(\d)\-(\d)(\d)""".r
+  val pattern(d1, d2, ch, d3, d4, d5, d6, d7) = StdIn.readLine()
+  val vowels = Set("A", "E", "I", "O", "U", "Y")
+
+  if ((d1.toInt + d2.toInt) % 2 == 0 
+      && (d3.toInt + d4.toInt) % 2 == 0 
+      && (d4.toInt + d5.toInt) % 2 == 0 
+      && (d6.toInt + d7.toInt) % 2 == 0 
+      && !vowels.contains(ch)) {
+    println("valid")
+  } else {
+    println("invalid")
+  }
+}
