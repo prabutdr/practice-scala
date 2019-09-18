c390a966295a1b7e6a32b72b46d95c55e5b4b5e2
prabutdr <prabutdr@gmail.com>
2019-09-18 21:15:58 +0530

    Day 12 - 3

---
+++ DSAMadeEasy/src/com/pj2r/dsame/tree/P38_BTFromPreOrder.scala
@@ -0,0 +1,21 @@
+package com.pj2r.dsame.tree
+
+object P38_BTFromPreOrder extends App {
+  var i: Int = 0
+  def buildTree(preOrder: String): BinaryTreeNode = {
+    if (i >= preOrder.length())
+      return null
+
+    val node = new BinaryTreeNode(preOrder(i), null, null)
+    if (preOrder(i) == 'L')
+      return node
+    i += 1
+    node.left = buildTree(preOrder)
+    i += 1
+    node.right = buildTree(preOrder)
+    node
+  }
+
+  // TEST
+  println(buildTree("IILLILL"))
+}
